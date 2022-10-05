371832f4613b6d3755496135f1f9222c4546e3db
Quarsatos <manuelpapula69@gmail.com>
2022-10-05 11:10:54 +0200

    Basic Exercise 1 complete

--- Papula/basic-exercises/script/script.js
+++ Papula/basic-exercises/script/script.js
@@ -1,1 +1,14 @@
 "use strict";
+// Exercise 1
+// let num: number = 1;
+// for(let i=0; i <=10; i++){
+//     let result  = num * i;
+//     console.log(num + " x " + i +  " = " + result) 
+// }
+// for(let i = 0; i <=10; i++){
+//     for(let k = 0; k <= 10; k++){
+//         let result = i * k;
+//         console.log(i + " x " + k + " = " + result);
+//         console.log("-----------")
+//     }
+// }
