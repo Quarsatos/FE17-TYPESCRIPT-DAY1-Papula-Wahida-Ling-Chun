bb8ce074777eeb677bd9c3cf717680a51fbcbf95
Quarsatos <manuelpapula69@gmail.com>
2022-10-05 11:50:37 +0200

    modified exercise 1

--- Papula/basic-exercises/script/script.ts
+++ Papula/basic-exercises/script/script.ts
@@ -1,17 +1,29 @@
 // Exercise 1
 
-// let num: number = 1;
+// let num: number = 0;
 
 // for(let i=0; i <=10; i++){
 //     let result  = num * i;
-//     console.log(num + " x " + i +  " = " + result) 
+//     console.log(`${num} x ${i} = ${result}`);
 // }
 
+
 // for(let i = 0; i <=10; i++){
+//     console.log(`-------------------`)    
+//     console.log(`started block ${i}`)    
+//     console.log(`-------------------`)
 //     for(let k = 0; k <= 10; k++){
 //         let result = i * k;
-//         console.log(i + " x " + k + " = " + result);
-//         console.log("-----------")
+//         console.log(`${i} x ${k} = ${result}`);
+//         // console.log("-----------")
 //     }
+//     console.log(`-------------------`)    
+//     console.log(`completed block ${i}`)    
+//     console.log(`-------------------`)    
 // }
+
+
+
+
+
 
