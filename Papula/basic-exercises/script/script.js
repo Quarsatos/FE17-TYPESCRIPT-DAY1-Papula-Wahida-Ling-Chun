6fc0c1731535d28abb58a3aaf07eeba893decb88
Quarsatos <manuelpapula69@gmail.com>
2022-10-05 13:31:01 +0200

    basic exercise 3 complete

--- Papula/basic-exercises/script/script.js
+++ Papula/basic-exercises/script/script.js
@@ -6,31 +6,38 @@
 //     console.log(`${num} x ${i} = ${result}`);
 // }
 // for(let i = 0; i <=10; i++){
-//     console.log(`-------------------`)    
-//     console.log(`started block ${i}`)    
-//     console.log(`-------------------`)
+//     console.log(`-------------------`);    
+//     console.log(`started block ${i}`);    
+//     console.log(`-------------------`);
 //     for(let k = 0; k <= 10; k++){
 //         let result = i * k;
 //         console.log(`${i} x ${k} = ${result}`);
-//         // console.log("-----------")
+//         // console.log("-----------");
 //     }
-//     console.log(`-------------------`)    
-//     console.log(`completed block ${i}`)    
-//     console.log(`-------------------`)    
+//     console.log(`-------------------`);    
+//     console.log(`completed block ${i}`);    
+//     console.log(`-------------------`);    
 // }
 // Exercise 2
-let names = { fname: "Manuel", lname: "Papula" };
-let nameArray = Object.values(names);
-document.write(`${names.fname} ${names.lname} <br><br>`);
-for (let i = 0; i <= 10; i++) {
-    document.write(`${nameArray[0]} ${i} <br>`);
-}
-let test = function () {
-    for (let i = 0; i <= 10; i++) {
-        setTimeout(() => {
-            console.log(`${nameArray[1]} ${i}`);
-        }, 5000);
-    }
-};
-test();
+// let names = {fname: "Manuel", lname: "Papula"};
+// let nameArray = Object.values(names);
+// document.write(`${names.fname} ${names.lname} <br><br>`);
+// for(let i=0; i<=10;i++){
+//     document.write(`${nameArray[0]} ${i} <br>`);
+// }
+// let test: () => void = function(){
+//     for(let i=0; i<=10;i++){
+//         setTimeout(() =>  {
+//             console.log(`${nameArray[1]} ${i}`);
+//         },5000);
+//     }    
+// }
+// test()
 // Exercise 3
+let namesArr = ["John", "Mike", "Franziska", "Maria"];
+console.log(namesArr);
+for (let i = 0; i < namesArr.length; i++) {
+    console.log(i);
+    console.log(`${namesArr[i]}`);
+    console.log(`${namesArr[i]} ${i}`);
+}
--- Papula/basic-exercises/script/script.ts
+++ Papula/basic-exercises/script/script.ts
@@ -9,42 +9,51 @@
 
 
 // for(let i = 0; i <=10; i++){
-//     console.log(`-------------------`)    
-//     console.log(`started block ${i}`)    
-//     console.log(`-------------------`)
+//     console.log(`-------------------`);    
+//     console.log(`started block ${i}`);    
+//     console.log(`-------------------`);
 //     for(let k = 0; k <= 10; k++){
 //         let result = i * k;
 //         console.log(`${i} x ${k} = ${result}`);
-//         // console.log("-----------")
+//         // console.log("-----------");
 //     }
-//     console.log(`-------------------`)    
-//     console.log(`completed block ${i}`)    
-//     console.log(`-------------------`)    
+//     console.log(`-------------------`);    
+//     console.log(`completed block ${i}`);    
+//     console.log(`-------------------`);    
 // }
 
 // Exercise 2
 
-let names = {fname: "Manuel", lname: "Papula"}
+// let names = {fname: "Manuel", lname: "Papula"};
 
-let nameArray = Object.values(names)
+// let nameArray = Object.values(names);
 
-document.write(`${names.fname} ${names.lname} <br><br>`)
+// document.write(`${names.fname} ${names.lname} <br><br>`);
 
-for(let i=0; i<=10;i++){
-    document.write(`${nameArray[0]} ${i} <br>`)
-}
+// for(let i=0; i<=10;i++){
+//     document.write(`${nameArray[0]} ${i} <br>`);
+// }
 
-let test: () => void = function(){
-    for(let i=0; i<=10;i++){
-        setTimeout(() =>  {
-            console.log(`${nameArray[1]} ${i}`)
-        },5000);
-    }    
-}
-test()
+// let test: () => void = function(){
+//     for(let i=0; i<=10;i++){
+//         setTimeout(() =>  {
+//             console.log(`${nameArray[1]} ${i}`);
+//         },5000);
+//     }    
+// }
+// test()
 
 // Exercise 3
 
+let namesArr: string[] = ["John", "Mike", "Franziska", "Maria"];
+
+console.log(namesArr)
+
+for(let i=0; i < namesArr.length; i++){
+    console.log(i);
+    console.log(`${namesArr[i]}`);
+    console.log(`${namesArr[i]} ${i}`);
+}
 
 
 
